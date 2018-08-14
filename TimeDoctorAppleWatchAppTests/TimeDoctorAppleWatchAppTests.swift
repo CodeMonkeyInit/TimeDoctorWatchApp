3d273d9d13dc54b11c1b19bcbc71f31afb0f51bf
deniskuliev <kyliev09@gmail.com>
2018-08-14 23:24:08 +0300

    Initial Commit

---
+++ TimeDoctorAppleWatchAppTests/TimeDoctorAppleWatchAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TimeDoctorAppleWatchAppTests.swift
+//  TimeDoctorAppleWatchAppTests
+//
+//  Created by Денис Кулиев on 14.08.2018.
+//  Copyright © 2018 Денис Кулиев. All rights reserved.
+//
+
+import XCTest
+@testable import TimeDoctorAppleWatchApp
+
+class TimeDoctorAppleWatchAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
